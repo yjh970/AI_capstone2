074ca6631d6eea7a66956307e90f611cd90dc672
06kimr <06kimr@gmail.com>
2021-11-04 02:48:18 +0900

    2021-11-04 미팅세부정보 탭전환성공

--- front/ricky/lib/screens/details/components/product_description.dart
+++ front/ricky/lib/screens/details/components/product_description.dart
@@ -1,12 +1,10 @@
 import 'package:final_project/models/Product.dart';
 import 'package:final_project/screens/details/components/product_description_meeting.dart';
-import 'package:final_project/screens/details/components/product_description_senior.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 import '../../../constants.dart';
 import '../../../size_config.dart';
-import 'categories.dart';
 
 class ProductDescription extends StatefulWidget {
   const ProductDescription({
@@ -28,59 +26,56 @@ class _ProductDescriptionState extends State<ProductDescription> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.symmetric(
-                horizontal: getProportionateScreenWidth(20)),
-            child: Text(widget.product.title,
-                style: Theme.of(context).textTheme.headline6),
-          ),
-          const SizedBox(height: 1),
-          Align(
-            alignment: Alignment.centerRight,
-            child: Container(
-              padding: EdgeInsets.all(getProportionateScreenWidth(15)),
-              width: getProportionateScreenWidth(64),
-              decoration: BoxDecoration(
-                  color: widget.product.isFavorite
-                      ? Color(0xFFFFE6E6)
-                      : Color(0xFFF5F6F9),
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      bottomLeft: Radius.circular(20))),
-              child: SvgPicture.asset(
-                'assets/icons/Heart Icon_2.svg',
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: getProportionateScreenWidth(20)),
+          child: Text(widget.product.title,
+              style: Theme.of(context).textTheme.headline6),
+        ),
+        const SizedBox(height: 1),
+        Align(
+          alignment: Alignment.centerRight,
+          child: Container(
+            padding: EdgeInsets.all(getProportionateScreenWidth(15)),
+            width: getProportionateScreenWidth(64),
+            decoration: BoxDecoration(
                 color: widget.product.isFavorite
-                    ? Color(0xFFFF4848)
-                    : Color(0xFFDBDEE4),
-              ),
+                    ? Color(0xFFFFE6E6)
+                    : Color(0xFFF5F6F9),
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    bottomLeft: Radius.circular(20))),
+            child: SvgPicture.asset(
+              'assets/icons/Heart Icon_2.svg',
+              color: widget.product.isFavorite
+                  ? Color(0xFFFF4848)
+                  : Color(0xFFDBDEE4),
             ),
           ),
-          SizedBox(
-            height: getProportionateScreenWidth(20),
-          ),
-          Padding(
-            padding:
-                EdgeInsets.symmetric(vertical: getProportionateScreenWidth(20)),
-            child: SizedBox(
-              height: 25,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: categories.length,
-                itemBuilder: (context, index) {
-                  return buildCategory(index);
-                },
-              ),
+        ),
+        SizedBox(
+          height: getProportionateScreenWidth(20),
+        ),
+        Padding(
+          padding:
+              EdgeInsets.symmetric(vertical: getProportionateScreenWidth(20)),
+          child: SizedBox(
+            height: 25,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: categories.length,
+              itemBuilder: (context, index) {
+                return buildCategory(index);
+              },
             ),
           ),
-          //tab index가 바뀔 때 내용이 바뀌게끔 해야됨
-          ProductDescriptionMeeting(product: widget.product),
-          ProductDescriptionSenior(product: widget.product),
-        ],
-      ),
+        ),
+        //tab index가 바뀔 때 내용이 바뀌게끔 해야됨
+        ProductDescriptionMeeting(product: widget.product),
+      ],
     );
   }
 
--- front/ricky/lib/screens/details/components/product_description_meeting.dart
+++ front/ricky/lib/screens/details/components/product_description_meeting.dart
@@ -3,54 +3,37 @@ import 'package:flutter/material.dart';
 
 import '../../../constants.dart';
 
-class ProductDescriptionMeeting extends StatefulWidget {
-  const ProductDescriptionMeeting({Key? key, required this.product,})
-      : super(key: key);
+class ProductDescriptionMeeting extends StatelessWidget {
+  const ProductDescriptionMeeting({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
 
   final Product product;
 
   @override
-  _ProductDescriptionMeetingState createState() =>
-      _ProductDescriptionMeetingState();
-}
-
-class _ProductDescriptionMeetingState extends State<ProductDescriptionMeeting> {
-  bool isReadMore = false;
-
-  @override
   Widget build(BuildContext context) {
-    final maxLines = isReadMore ? 100 : 2;
-    return  Padding(
+    return Padding(
       padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
       child: Column(
         children: [
-          Text(
-            widget.product.description,
-            maxLines: maxLines,
-            overflow: TextOverflow.ellipsis,
-          ),
-          GestureDetector(
-            onTap: () => setState(() => isReadMore = !isReadMore),
-            child: Row(
-              children: [
-                Text(
-                  isReadMore ? "See less Detail" : "See more Detail",
-                  style: TextStyle(
-                    color: kPrimaryColor,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                SizedBox(width: 5),
-                Icon(
-                  Icons.arrow_forward,
-                  size: 12,
-                  color: kPrimaryColor,
-                ),
-              ],
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              '강의소개\n',
+              style: TextStyle(fontSize: 20, color: Colors.black,
+                  decoration: TextDecoration.underline),
+              textAlign: TextAlign.end,
             ),
+          ),
+          Text(
+            product.meeting_description,
+            maxLines: 1000,
+            overflow: TextOverflow.ellipsis,
           ),
         ],
       ),
     );
   }
 }
+
