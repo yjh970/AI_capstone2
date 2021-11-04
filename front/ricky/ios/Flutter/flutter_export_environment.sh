f76f2a7b7cb420570ebb4aa11f8dd6c90511ece8
JeongHeunLab <yeonjh970716@gmail.com>
2021-11-04 16:33:27 +0900

    create master source code. ricky -> master

--- front/ricky/ios/Flutter/flutter_export_environment.sh
+++ front/ricky/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter_windows_2.5.2-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\User\StudioProjects\Final_Project"
+export "FLUTTER_ROOT=C:\Users\JY\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\JY\Desktop\yjh\git\AI_capstone2\front\ricky"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
